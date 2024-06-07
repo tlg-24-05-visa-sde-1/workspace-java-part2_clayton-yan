cc64b27807a37c8bb446c8bc7296ff03efd60f0d
Clayton Yan <claytony@tlgcohort.com>
2024-06-07 16:22:36 -0500

    generics lab

--- Lab04.2/test/com/javatunes/product/OrderTest.java
+++ Lab04.2/test/com/javatunes/product/OrderTest.java
@@ -41,5 +41,13 @@ public class OrderTest {
     @Test
     public void testProcessCart() {
 
+        Order order1 = new Order("order-1");
+        order1.processCart(productCart);
+
+        Order order2 = new Order("order-2");
+        order2.processCart(musicCart);
+
+        Order order3 = new Order("order-3");
+        order3.processCart(mediaCart);
     }
 }
