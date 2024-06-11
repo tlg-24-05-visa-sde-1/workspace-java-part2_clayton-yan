b2626dda9801b0ee1f1431ec2117dd66bd33ab65
Clayton Yan <claytony@tlgcohort.com>
2024-06-11 17:11:06 -0500

    Member-level true inner class Tuner and static nested classes DisplayType, ChannelComparator, and BrandChannelComparator

--- Lab06.1/src/com/entertainment/Television.java
+++ Lab06.1/src/com/entertainment/Television.java
@@ -8,6 +8,7 @@
  */
 package com.entertainment;
 
+import java.util.Comparator;
 import java.util.Objects;
 
 public class Television implements Comparable<Television> {
@@ -113,5 +114,45 @@ public class Television implements Comparable<Television> {
                      Objects.equals(this.getDisplay(), other.getDisplay());
         }
         return result;
+    }
+    // member level named inner classes
+    private class Tuner {
+        private int channel = 3;  // default channel for cable and satellite customers
+
+        public int getChannel() {
+            return this.channel;
+        }
+
+        public void setChannel(int channel) {
+            this.channel = channel;
+        }
+    }
+
+    // static nested classes, referenced as Television. something
+    // Television.DisplayType
+    public static enum DisplayType {
+        LCD, LED, OLED, PLASMA, CRT
+    }
+
+    // Television.ChannelComparator
+    public static class ChannelComparator implements Comparator<Television> {
+
+        @Override
+        public int compare(Television tv1, Television tv2) {
+            return Integer.compare(tv1.getCurrentChannel(), tv2.getCurrentChannel());
+        }
+    }
+
+    public static class BrandChannelComparator implements Comparator<Television> {
+
+        @Override
+        public int compare(Television tv1, Television tv2) {
+            int result = tv1.getBrand().compareTo(tv2.getBrand());
+
+            if (result == 0) {
+                result = Integer.compare(tv1.getCurrentChannel(), tv2.getCurrentChannel());
+            }
+            return result;
+        }
     }
 }
