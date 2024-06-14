d52c1d66054996cb5373dd87a5d4fe05c8b469ef
Clayton Yan <claytony@tlgcohort.com>
2024-06-14 10:42:18 -0500

    added pickle getters/setters

--- Scratch/src/com/crisp/Pickle.java
+++ Scratch/src/com/crisp/Pickle.java
@@ -2,4 +2,12 @@ package com.crisp;
 
 class Pickle {
     private int sourness;
+
+    public int getSourness() {
+        return sourness;
+    }
+
+    public void setSourness(int sourness) {
+        this.sourness = sourness;
+    }
 }
